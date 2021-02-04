1b9e5a06b73fa32f9834deb6fdc9936af302c0b8
solano <fasolanof@gmail.com>
2021-02-04 10:36:47 -0600

    tarea 1

---
+++ database/migrations/2021_02_04_163236_create_users_domicilio_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_domicilio', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('domicilio', 100);
+            $table->string('numero_exterior', 10);
+            $table->string('colonia', 60);
+            $table->string('cp', 10);
+            $table->string('ciudad', 60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_domicilio');
+    }
+}
